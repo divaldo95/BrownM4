5a18f055d1fd02ec21ae39151f52ff09d9829b8e
David Baranyai <divaldo95@gmail.com>
2017-10-30 12:29:43 +0100

    Version 1.1

--- include/BM1DProcess.hh
+++ include/BM1DProcess.hh
@@ -11,10 +11,13 @@
 class BM1DProcess {
 public:
   BM1DProcess(Int_t n); //n is the number of steps
+  BM1DProcess(Int_t n, Double_t mean, Double_t sigma); //n is the number of steps, mean and sigma are for Gauss
   ~BM1DProcess();
   
-  void SetP0(Double_t p0_new){p0 = p0_new;}
-  void SetP1(Double_t p1_new){p1 = p1_new;}
+  void SetP0(Double_t p0_new);
+  void SetP1(Double_t p1_new);
+  void SetSigma(Double_t sigma_new);
+  void SetMean(Double_t mean_new);
 
   void Init();
   void Run();
@@ -25,8 +28,9 @@ public:
  private:
   Int_t nSteps;
   Double_t p0,p1,p2,p3,p4;
+  Double_t mean,sigma;
   TRandom* randomGenerator;
-  Double_t rand1;
+  Double_t rand1,rand2;
   std::vector<Double_t> t;
   std::vector<Double_t> x;
 };
--- src/BM1DProcess.cc
+++ src/BM1DProcess.cc
@@ -1,25 +1,37 @@
 #include "BM1DProcess.hh"
 
 BM1DProcess::BM1DProcess(Int_t nP) : 
-  nSteps (nP), p0(0.5), p1(0.3)
+nSteps (nP), p0(0.5), p1(0.3), mean(0), sigma(1)
+{
+randomGenerator = new TRandom();
+}
+
+BM1DProcess::BM1DProcess(Int_t nP, Double_t mean_init, Double_t sigma_init) : 
+  nSteps (nP), p0(0.5), p1(0.3), mean(mean_init), sigma(sigma_init)
 {
   randomGenerator = new TRandom();
 }
 
 BM1DProcess::~BM1DProcess()
 {
-  ;
+	
 }
 
 void BM1DProcess::Init(){
-  t.push_back(0.0);  //let's start at t=0, x=0, you can change it if you vant, please use Set methods
+  t.push_back(0.0);  //let's start at t=0, x=0, you can change it if you want, please use Set methods
   x.push_back(0.0);
 }
+
+void BM1DProcess::SetP0(Double_t p0_new){p0 = p0_new;}
+void BM1DProcess::SetP1(Double_t p1_new){p1 = p1_new;}
+void BM1DProcess::SetSigma(Double_t sigma_new){sigma=sigma_new;}
+void BM1DProcess::SetMean(Double_t mean_new){mean=mean_new;}
 
 void BM1DProcess::Run(){
   for (Int_t i = 1;i < nSteps;i++)
     {
-      rand1 = randomGenerator->Uniform();
+			rand1 = randomGenerator->Uniform();
+			rand2 = randomGenerator->Gaus(mean,sigma);
 	if(rand1 < p0) {  //step in time, but no step in x
 	  t.push_back(t[i-1] + 1);
 	  x.push_back(x[i-1]);
@@ -28,12 +40,12 @@ void BM1DProcess::Run(){
 	  if(rand1 < p0 + p1)
 	    {
 	      t.push_back(t[i-1]+1);
-	      x.push_back(x[i-1]+1); //one step right
+	      x.push_back(x[i-1]+rand2); //one step right
 	    }
 	  else
 	    {
 	      t.push_back(t[i-1]+1);
-	      x.push_back(x[i-1]-1); //one step left
+	      x.push_back(x[i-1]-rand2); //one step left
 	    }
 	}
     }
